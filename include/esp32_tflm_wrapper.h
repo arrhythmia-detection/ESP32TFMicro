e1fa118debc0a1329b3d16967ca992be6e11d3ab
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2024-10-29 12:33:23 +0600

    feat(wip): added all the necessary wrappers for esp32 boards
    I have followed similar design pattern of EloquentArduino and created this lib based on his custom fork of tflm wrapper. I have yet to make necessary changes to make it print per layer wise/ops wise execution time.

---
+++ include/esp32_tflm_wrapper.h
@@ -0,0 +1,11 @@
+//
+// Created by Inmoresentum on 10/29/24.
+//
+
+#ifndef ESP32_TFLM_WRAPPER_H
+#define ESP32_TFLM_WRAPPER_H
+
+#include <tflm_esp32.h>
+#include <eloquent_tinyml.h>
+
+#endif //ESP32_TFLM_WRAPPER_H
